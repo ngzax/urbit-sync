1d45a6525b13d3a397e3789e300ef36341c46d0a
Daryl Richter <daryl@ngzax.com>
2017-09-21 18:37:44 -0400

    puts a wrapper func around config load
    this will let me exit gracefully if the config doesn’t parse.

--- urbsync.rb
+++ urbsync.rb
@@ -6,40 +6,28 @@ require 'pathname'
 def load_config
   require 'yaml'
 
-  config = YAML.load_file('_config.yml')
+  config_file = YAML.load_file('_config.yml')
 
-  desks = config['desks']
-  paths = config['paths']
-  pier = config['pier']
-  watch_dirs = config['watch_dirs']
+  config = {}
+  config['desks']      = config_file['desks']
+  config['paths']      = config_file['paths']
+  config['pier']       = config_file['pier']
+  config['watch_dirs'] = config_file['watch_dirs']
+  config
 end
 
-FileWatcher.new(watch_dirs).watch do |fn, ev|
-  p = Pathname.new(fn)
-  watch_dir = watch_dirs.find {|d| p.realpath.to_s.include?(d)}
-  index = watch_dirs.index(watch_dir)
+c = load_config
+unless (c.empty?)
+  FileWatcher.new(c['watch_dirs']).watch do |fn, ev|
+    p = Pathname.new(fn)
+    watch_dir = c['watch_dirs'].find {|d| p.realpath.to_s.include?(d)}
+    index = c['watch_dirs'].index(watch_dir)
 
-  if(ev == :changed)
-    rel_path = p.realpath.sub(watch_dirs[index], '').sub(fn, '')
-    cmd = "cp -af #{fn} #{pier}#{desks[index]}#{paths[index]}#{rel_path}"
+    if(ev == :changed)
+      rel_path = p.realpath.sub(c['watch_dirs'][index], '').sub(fn, '')
+      cmd = "cp -af #{fn} #{c['pier']}#{c['desks'][index]}#{c['paths'][index]}#{rel_path}"
+    end
+    p "--> #{cmd}"
+    system(cmd)
   end
-  p "--> #{cmd}"
-  system(cmd)
 end
-
-
-
-# fn, ev = f.shift
-# puts "f: #{fn}"
-# puts "e: #{ev}"
-
-# puts "Basename         : #{p.basename}"
-# puts "Relative filename: #{File.join('.', p)}"
-# puts "Absolute filename: #{p.realpath}"
-
-# This is the old watchr stuff, just haven't deleted it yet.
-# ENV["WATCHR"] = "1"
-
-# watch( 'urbit-docs/docs.md' ) {|f| system("cp -f #{f[0]} ../../urbits/socbud_fallen/home/web/")}
-# watch( 'urbit-docs/(.*).md' ) {|f| puts "#{f[0]} -- #{f[1]}"}
-# watch( 'urbit-docs/(.*).md' ) {|f| system("cp -f #{f[0]} ../urbits/socbud_fallen/home/web/#{f[1]}.md")}
