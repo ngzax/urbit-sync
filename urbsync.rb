f9e5fde95f394ee084904871663fb02d7c2e902a
Daryl Richter <daryl@ngzax.com>
2017-09-22 18:14:25 -0400

    Added a simple ability to exclude files.

--- urbsync.rb
+++ urbsync.rb
@@ -9,10 +9,11 @@ def load_config
   config_file = YAML.load_file('_config.yml')
 
   config = {}
-  config['desks']      = config_file['desks']
-  config['paths']      = config_file['paths']
-  config['pier']       = config_file['pier']
-  config['watch_dirs'] = config_file['watch_dirs']
+  config['desks']          = config_file['desks']
+  config['excluded_files'] = config_file['excluded_files']
+  config['paths']          = config_file['paths']
+  config['pier']           = config_file['pier']
+  config['watch_dirs']     = config_file['watch_dirs']
   config
 end
 
@@ -20,14 +21,18 @@ c = load_config
 unless (c.empty?)
   Filewatcher.new(c['watch_dirs']).watch do |fn, ev|
     p = Pathname.new(fn)
-    watch_dir = c['watch_dirs'].find {|d| p.realpath.to_s.include?(d)}
-    index = c['watch_dirs'].index(watch_dir)
+    unless c['excluded_files'].include?(p.basename.to_s)
+      watch_dir = c['watch_dirs'].find {|d| p.realpath.to_s.include?(d)}
+      index = c['watch_dirs'].index(watch_dir)
 
-    if(ev == :updated)
-      rel_path = p.realpath.sub(c['watch_dirs'][index], '').sub(fn, '')
-      cmd = "cp -af #{fn} #{c['pier']}#{c['desks'][index]}#{c['paths'][index]}#{rel_path}"
+      if(ev == :updated)
+        rel_path = p.realpath.sub(c['watch_dirs'][index], '').sub(fn, '')
+        cmd = "cp -af #{fn} #{c['pier']}#{c['desks'][index]}#{c['paths'][index]}#{rel_path}"
+      end
+      p "--> #{cmd}"
+      system(cmd) if cmd
+    else
+      p "Excluded, not copied --> #{fn}"
     end
-    p "--> #{cmd}"
-    system(cmd) if cmd
   end
 end
