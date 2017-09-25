a45ad70c26dfd4a04bd3c2674a15c0e2fd2bd649
Daryl Richter <daryl@ngzax.com>
2017-09-25 18:41:05 -0400

    Stubbed out method for initialization

--- urbsync.rb
+++ urbsync.rb
@@ -17,22 +17,31 @@ def load_config
   config
 end
 
-c = load_config
-unless (c.empty?)
-  Filewatcher.new(c['watch_dirs']).watch do |fn, ev|
-    p = Pathname.new(fn)
-    unless c['excluded_files'].include?(p.basename.to_s)
-      watch_dir = c['watch_dirs'].find {|d| p.realpath.to_s.include?(d)}
-      index = c['watch_dirs'].index(watch_dir)
+def init
+  p "Initializing all watch directories"
+  exit 0
+end
 
-      if(ev == :updated)
-        rel_path = p.realpath.sub(c['watch_dirs'][index], '').sub(fn, '')
-        cmd = "cp -af #{fn} #{c['pier']}#{c['desks'][index]}#{c['paths'][index]}#{rel_path}"
+def sync
+  c = load_config
+  unless (c.empty?)
+    Filewatcher.new(c['watch_dirs']).watch do |fn, ev|
+      p = Pathname.new(fn)
+      unless c['excluded_files'].include?(p.basename.to_s)
+        watch_dir = c['watch_dirs'].find {|d| p.realpath.to_s.include?(d)}
+        index = c['watch_dirs'].index(watch_dir)
+
+        if(ev == :updated)
+          rel_path = p.realpath.sub(c['watch_dirs'][index], '').sub(fn, '')
+          cmd = "cp -af #{fn} #{c['pier']}#{c['desks'][index]}#{c['paths'][index]}#{rel_path}"
+        end
+        p "--> #{cmd}"
+        system(cmd) if cmd
+      else
+        p "Excluded, not copied --> #{fn}"
       end
-      p "--> #{cmd}"
-      system(cmd) if cmd
-    else
-      p "Excluded, not copied --> #{fn}"
     end
   end
 end
+
+('--init' == ARGV[0]) ? init : sync
