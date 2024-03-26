a5ff820a617635e03ba88413ac833fcd23d485c8
Daryl Richter <daryl@deliverycircle.com>
2024-03-26 16:12:22 -0400

    basic multi-pier config

--- urbsync.rb
+++ urbsync.rb
@@ -10,22 +10,24 @@ def load_config
   config_file = YAML.load_file('_config.yml')
 
   config = {}
+  config['piers'] = []
 
   ships = config_file['ships']
   ships.each do |ship|
-    config['pier']           = ship['ship']['pier']
-    config['excluded_files'] = ship['ship']['excluded_files']
+    config['ship']           = ship['ship']
+    config['piers']          << ship['pier']
+    config['excluded_files'] = ship['excluded_files']
 
     config['watch_dirs'] = []
     config['desks']      = []
     config['paths']      = []
 
-    watches = ship['ship']['watch']
+    watches = ship['apps']
     watches.each do |app|
-      root = app['app']['root']
-      config['watch_dirs'] << app['app']['comp'].map {|a| "#{root}#{a['src']}"}
-      config['desks']      << app['app']['comp'].map {|a| "#{a['desk']}"}
-      config['paths']      << app['app']['comp'].map {|a| "#{a['path']}"}
+      root = app['root']
+      config['watch_dirs'] << app['comp'].map {|a| "#{root}#{a['src']}"}
+      config['desks']      << app['comp'].map {|a| "#{a['desk']}"}
+      config['paths']      << app['comp'].map {|a| "#{a['path']}"}
     end
   end
 
@@ -37,7 +39,7 @@ def load_config
 end
 
 def assemble_path(pier, desk, path, rel_path)
-  to_path = "#{pier}#{desk}#{path}#{rel_path}"
+  to_path = "#{pier}#{desk}#{path}/#{rel_path}"
   if !Dir.exist?(to_path)
     cmd = "mkdir #{to_path}"
     puts "--> #{cmd}"
@@ -51,14 +53,16 @@ def copy(config, index, from_path)
   unless ('.' == from_path.basename.to_s[0]) || config['excluded_files'].include?(to_file)
     unless Pathname.new(from_path).symlink?
       rel_path = from_path.realpath.sub(config['watch_dirs'][index], '').sub(to_file, '')
-      to_path = assemble_path(config['pier'], config['desks'][index], config['paths'][index], rel_path)
-      cmd = "cp -af #{from_path} #{to_path}"
-      puts "--> #{cmd}" if config[:verbose]
+      config['piers'].each do |pier|
+        to_path = assemble_path(pier, config['desks'][index], config['paths'][index], rel_path)
+        cmd = "cp -af #{from_path} #{to_path}"
+        puts "--> #{cmd}" if config[:verbose]
+        system(cmd) if cmd
+      end
     end
   else
     puts "Excluded, not copied --> #{to_file}" if config[:verbose]
   end
-  system(cmd) if cmd
 end
 
 def init(c)
@@ -77,7 +81,11 @@ end
 def sync(c)
   if c[:verbose]
     puts "Synchronizing all watch directories..."
-    c['watch_dirs'].each_with_index {|w, i| puts "#{w} --> #{c['pier']}#{c['desks'][i]}#{c['paths'][i]}"}
+    c['watch_dirs'].each_with_index {|w, i|
+      c['piers'].each do |pier|
+        puts "#{w} --> #{pier}#{c['desks'][i]}#{c['paths'][i]}"
+      end
+    }
   end
   Filewatcher.new(c['watch_dirs'], every: true).watch do |file_events|
     file_events.each_pair do |to_file, event|
